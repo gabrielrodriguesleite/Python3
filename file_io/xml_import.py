db5d9e6ea3158e13c5e471a2a8c74c083f1bc500
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 23:33:53 -0300

    [feat] test xml2arr

--- file_io/xml_import.py
+++ file_io/xml_import.py
@@ -3,3 +3,22 @@ import xml.etree.cElementTree as et
 
 def xml2arr(f):
     return [{e.tag: e.text for e in r} for r in et.parse(f).getroot()]
+
+
+dataTestXML = [
+    {},
+    {"name": "main_window", "width": "500", "height": "500"},
+    {"hOffset": "250", "vOffset": "250", "alignment": "center"},
+    {
+        "name": "text1",
+        "hOffset": "250",
+        "vOffset": "100",
+        "alignment": "center",
+        "onMouseUp": "\n            sun1.opacity = (sun1.opacity / 100) * 90;\n        ",
+    },
+]
+
+
+def test_xml2arr():
+    with open("./file_io/dataTest/dados.xml") as f:
+        assert xml2arr(f) == dataTestXML
