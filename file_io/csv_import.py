562f7d0fc3bdfed6694e715d3facc592f36181bf
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 23:17:40 -0300

    [feat] test csv2arr

--- file_io/csv_import.py
+++ file_io/csv_import.py
@@ -14,3 +14,40 @@ def csv2arr(file, delimiter=","):
 
 def csv2arrV2(file):
     return [row for row in csv.DictReader(file)]
+
+
+dataTest = [
+    {
+        "Login email": "laura@example.com",
+        "Identifier": "2070",
+        "First name": "Laura",
+        "Last name": "Grey",
+    },
+    {
+        "Login email": "craig@example.com",
+        "Identifier": "4081",
+        "First name": "Craig",
+        "Last name": "Johnson",
+    },
+    {
+        "Login email": "mary@example.com",
+        "Identifier": "9346",
+        "First name": "Mary",
+        "Last name": "Jenkins",
+    },
+    {
+        "Login email": "jamie@example.com",
+        "Identifier": "5079",
+        "First name": "Jamie",
+        "Last name": "Smith",
+    },
+]
+
+
+def test_csv2arr():
+
+    with open("./file_io/dataTest/email.csv") as f:
+        list1 = csv2arr(f)
+
+    assert list1 == dataTest
+
