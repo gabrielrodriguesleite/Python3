b8700bb9356aba7a1fb0bbad0a1fde1ab79c28e0
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 23:07:19 -0300

    [feat] delimiter for csv exposed

--- file_io/csv_import.py
+++ file_io/csv_import.py
@@ -1,8 +1,9 @@
 import csv
 
-def csv2arr(file):
+
+def csv2arr(file, delimiter=","):
     result = []
-    header, *data = csv.reader(file, delimiter=",", quotechar='"')
+    header, *data = csv.reader(file, delimiter=delimiter, quotechar='"')
     for i in data:
         ob = {}
         for j in range(len(header)):
